f79b6a98e2520467edcb9db3b2c684ed971c0271
Mahir Molai <mahir@molai.dev>
2023-01-05 22:13:00 +0500

    Move TestComponent To Individual

--- tests/index.js
+++ tests/index.js
@@ -1,24 +1,6 @@
 const { App, Component } = require("../index");
-
-class AppComponent extends Component {
-  render() {
-    return `
-      <div class="App">
-        <h1>Hello, ${this.props.name}.</h1>
-        <br/>
-        <p>${this.state.message}</p>
-        <br/>
-        <i>${this.state.test}</i>
-      </div>
-    `;
-  }
-}
+const component = require("./TestComponent");
 
 const app = new App({ title: "Molax", style: "./app.css", script: "./script.js" });
-const component = new AppComponent({ name: "Molax" });
-component.setState({
-  message: "Welcome To Molax. Edit App.js to get started.",
-  test: "*passed from state*"
-});
 app.addComponent(component);
 app.renderToFile("./tests/index.html");
