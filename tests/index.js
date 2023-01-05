5a0ce57f1ccab181964af38fd9462b0c3ac595ac
Mahir Molai <mahir@molai.dev>
2023-01-05 19:59:11 +0500

    first commit

---
+++ tests/index.js
@@ -0,0 +1,24 @@
+const { App, Component } = require("../index");
+
+class AppComponent extends Component {
+  render() {
+    return `
+      <div class="App">
+        <h1>Hello, ${this.props.name}.</h1>
+        <br>
+        <p>${this.state.message}</p>
+        <br>
+        <i>${this.state.test}</i>
+      </div>
+    `;
+  }
+}
+
+const app = new App({ title: "Molax", style: "./app.css" });
+const component = new AppComponent({ name: "Molax" });
+component.setState({
+  message: "Welcome To Molax. Edit App.js to get started.",
+  test: "*passed from state*"
+});
+app.addComponent(component);
+app.renderToFile("./tests/index.html");
